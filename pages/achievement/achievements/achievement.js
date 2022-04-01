322b3db6a5d87ca00994e0f7ddd7245bcfc09327
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-04-01 13:03:29 +0800

    Fixed problem with progress bar not showing

--- pages/achievement/achievements/achievement.js
+++ pages/achievement/achievements/achievement.js
@@ -2,5 +2,5 @@ changeSize();
 window.addEventListener('resize', changeSize);
 
 function changeSize () {
-    document.getElementById('achiBlock').style.height = (document.getElementById('achiHeader').clientHeight + document.getElementById('myBar').clientHeight).toString() + 'px';
+    document.getElementById('achiBlock').style.height = document.getElementById('achiHeader').clientHeight + 'px';
 }
--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -2,11 +2,11 @@ changeSize();
 window.addEventListener('resize', changeSize);
 
 function changeSize () {
-    document.getElementById('contBlock').style.height = (document.getElementById('contHeader').clientHeight + document.getElementById('myBar').clientHeight).toString() + 'px';
+    document.getElementById('contBlock').style.height = document.getElementById('contHeader').clientHeight + 'px';
 }
 
 document.getElementById('form').addEventListener('submit', function (e) {
     var message = document.getElementById('message').value;
-    window.location.href = "mailto:hkpoverty@gmail.com?&subject="+encodeURIComponent("Message")+"&body="+encodeURIComponent(message);
+    window.location.href = "mailto:hkpoverty@gmail.com?&subject="+encodeURIComponent("Message")+"&body="+encodeURIComponent(email);
     e.preventDefault();
 })
