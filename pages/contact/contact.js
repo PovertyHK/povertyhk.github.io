3ecb02802893992ac24e7799709f8693e8244da2
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-04-01 10:31:08 +0800

    fixed some stuff

---
+++ pages/contact/contact.js
@@ -0,0 +1,6 @@
+changeSize();
+window.addEventListener('resize', changeSize);
+
+function changeSize () {
+    document.getElementById('contBlock').style.height = (document.getElementById('contHeader').clientHeight + document.getElementById('myBar').clientHeight).toString() + 'px';
+}
