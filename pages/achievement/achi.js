4ce56ef0277c6d2aed88c3ebd4a4e4eb43c6f35d
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-04-22 19:47:52 +0800

    made website compatible with keyboard-only users

--- pages/achievement/achi.js
+++ pages/achievement/achi.js
@@ -1,8 +1,35 @@
 function dropdown(item) {
 
     var className = ' ' + item.className + ' ';
+    var links = item.getElementsByClassName('achiLink')[0];
 
-    item.className = ~className.indexOf(' active ') ?
-                         className.replace(' active ', ' ') :
-                         item.className + ' active';
+    if (~className.indexOf(' active ')) {
+        item.className = className.replace(' active ', ' ');
+        links.tabIndex = -1;
+    } else {
+        item.className = className + ' active';
+        links.tabIndex = 0;
+    }
+}
+
+const events = ['click', 'touchend', 'keypress', 'onkeydown']
+
+var dropdownBlock = document.getElementsByClassName('achiWrap');
+
+for(let i=0; i < dropdownBlock.length; i++) {
+    events.forEach( function (evt) {
+        dropdownBlock[i].addEventListener(evt, function() {
+            dropdown(this);
+        }, false);
+    });
+}
+
+var dropdownLinks = document.getElementsByClassName('achiLink');
+
+for(let i=0; i < dropdownLinks.length; i++) {
+    events.forEach( function (evt) {
+        dropdownLinks[i].addEventListener(evt, function() {
+            window.location = 'achievements/'+dropdownLinks[i].getAttribute('name')+'.html';
+        }, false);
+    });
 }
